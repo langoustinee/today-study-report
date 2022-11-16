baef3f2fbbba00e38830469bdf9dcab9d007f358
jooney <xmun777@naver.com>
2022-11-17 08:40:59 +0900

    feat: worker.js update

--- resources/worker2.js
+++ resources/worker2.js
@@ -2,12 +2,10 @@ onmessage = (e) => {
     let res;
     setTimeout(() => {
         let start = e.data;
-        for (let i = 0; i < 1000000000; i++) { }
+        for (let i = 0; i < 10000000000; i++) { }
         const edt = Date.now();
         res = (edt - start);
         // 작업 결과를 html에게 전송한다.
         postMessage(res);
     }, 0);
-    
-    
 };
