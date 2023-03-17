a574f94bcbfe0a0fd24607200c9b687034d2e9eb
jooney <xmun777@naver.com>
2023-03-17 09:39:30 +0900

    2023-03-16 study record feat: Spring Validation app apply

---
+++ spring/validation-demo/src/test/java/com/kakao/lango/validation/ValidationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kakao.lango.validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
