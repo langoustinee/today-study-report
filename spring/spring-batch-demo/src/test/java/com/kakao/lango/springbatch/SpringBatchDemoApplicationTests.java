d50a123a71d34286d3217c147531f9d6edd0844b
jooney <xmun777@naver.com>
2023-03-23 15:44:06 +0900

    2023-03-23 study record feat: *.yml 암호화 및 복호화 설정 추가

---
+++ spring/spring-batch-demo/src/test/java/com/kakao/lango/springbatch/SpringBatchDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kakao.lango.springbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
